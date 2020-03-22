5ec1e51a344a3a04b58052de4f834c26a325e6d2
LNPappas <LPappas@gmail.com>
2020-03-21 20:18:08 -0700

    algoScripting: frankenSplice

--- algoScripting.js
+++ algoScripting.js
@@ -16,4 +16,17 @@ function titleCase(str) {
   console.log(u)
 
 
+function frankenSplice(arr1, arr2, n) {
+    arr2.splice(n,0,...arr1)
+  return arr2;
+}
+
+let f = frankenSplice([1, 2, 3], [4, 5], 1); //should return [4, 1, 2, 3, 5]
+let g = frankenSplice([1, 2], ["a", "b"], 1); //should return ["a", 1, 2, "b"]
+let h = frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) //should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"]
+console.log(f)
+console.log(g)
+console.log(h)
+
+
   
