b3b8c9e05e989f7416a51513529c84ff54a37b10
LNPappas <LPappas@gmail.com>
2020-03-23 17:45:59 -0700

    reduce, map, filter method

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -382,3 +382,15 @@ function sliceArray(anim, beginSlice, endSlice) {
     return averageRating;
   }
   console.log(getRating(watchList));
+
+  // Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+  // returns a new array containing only the square of only the 
+  // positive integers (decimal numbers are not integers) 
+  // when an array of real numbers is passed to it.
+  const squareList = (arr) => {
+    let a = arr.filter((a) => {if (a>=0 && Number.isInteger(a)) return a}).map(a => Math.pow(a,2));
+    return a;
+  };
+  
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  console.log(squaredIntegers);
