853ed8aff0827ce749b00ac70e1ab84b322bf73f
LNPappas <LPappas@gmail.com>
2020-03-30 12:42:23 -0700

    destroyer

--- algoScriptingIntermediate.js
+++ algoScriptingIntermediate.js
@@ -1,19 +1,11 @@
 function sumAll(arr) {
-    let start, end;
-    if (arr[0] < arr[1]){
-      start = arr[0];
-      end = arr[1];
-    }
-    else {
-      start = arr[1];
-      end = arr[0];
-    }
+    let start = Math.min(arr);
+    let end = Math.max(arr);
     let total = 0;
     while (start <= end){
       total += start
       start+=1;
     }
-  
     return total;
   }
   
@@ -34,3 +26,20 @@ function sumAll(arr) {
   
   let dA2 = diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
   console.log(dA2);
+
+  function destroyer() {
+    let arr = Array.prototype.slice.call(arguments);
+    let a = arr.shift()
+    return a.filter(item => !arr.includes(item))
+  }
+  
+  let dest = destroyer([1, 2, 3, 1, 2, 3], 2, 3); // should return [1,1]
+  console.log(dest);
+  let dest1 = destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); //should return [1, 5, 1]
+  console.log(dest1);
+  let dest2 = destroyer([3, 5, 1, 2, 2], 2, 3, 5); //should return [1]
+  console.log(dest2);
+  let dest3 = destroyer(["tree", "hamburger", 53], "tree", 53); //should return ["hamburger"]
+  console.log(dest3);
+  let dest4 = destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan"); // should return [12,92,65]
+  console.log(dest4);
