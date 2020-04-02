1eeb0b07b6a65631f6078c292d8433d4e893df48
LNPappas <LPappas@gmail.com>
2020-04-02 13:20:46 -0700

    spinalCase

--- algoScriptingIntermediate.js
+++ algoScriptingIntermediate.js
@@ -67,3 +67,23 @@ function sumAll(arr) {
   let w5 = whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 }) //should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }].
   let w6 = whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) //should return []
   console.log(w1, w2, w3, w4, w5, w6);
+
+  function spinalCase(str) {
+    //split string on space, underscore, or capitol letter (regex)
+    return str.split(/\s+|_+|(?=[A-Z])/)
+    //join split with dash
+    .join('-')
+    //convert to lowercase
+    .toLowerCase();
+  }
+  
+  let sC1 = spinalCase("This Is Spinal Tap") //should return "this-is-spinal-tap".
+  console.log(sC1);
+  let sC2 = spinalCase("thisIsSpinalTap") //should return "this-is-spinal-tap".
+  console.log(sC2);
+  let sC3 = spinalCase("The_Andy_Griffith_Show") //should return "the-andy-griffith-show".
+  console.log(sC3);
+  let sC4 = spinalCase("Teletubbies say Eh-oh") //should return "teletubbies-say-eh-oh".
+  console.log(sC4);
+  let sC5 = spinalCase("AllThe-small Things") //should return "all-the-small-things".
+  console.log(sC5);
